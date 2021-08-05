a6c255e5c50100d2a2e4517debe7b6467ebfc4a6
wConnor <connorga4@gmail.com>
2021-08-05 19:43:41 +0100

    created basic skeleton of program; checks for arguments

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <memory>
+#include <string>
+
+void usage(std::string program_name)
+{
+	std::cout << "pomocpp - a basic pomodoro timer written in cpp." << std::endl;
+	std::cout << "Usage: " << program_name << " <options>" << std::endl;
+	std::cout << "Available options:" << std::endl;
+	std::cout << "\tstart <name> - starts the given pomodoro session" << std::endl;
+	std::cout << "\tcreate <name> <time> <break_time> <count> - creates a new pomodoro" << std::endl;
+	std::cout << "\tdel <name> - deletes the specified pomodoro" << std::endl;
+	std::cout << "\tlist - lists created pomodoros" << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+	std::vector<std::string> args;
+	for (int i = 0; i != argc; ++i) {
+		args.push_back(static_cast<std::string>(argv[i]));
+	}
+
+	if (argc >= 2) {
+		if (args[1] == "start") {
+			
+		} else if (args[1] == "create") {
+			
+		} else if (args[1] == "del") {
+			
+		} else if (args[1] == "list") {
+			
+		} else {
+			usage(args[0]);
+		}
+
+	} else {
+		usage(args[0]);
+	}
+	
+    return 0;
+}
