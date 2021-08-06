5fcb0f4be3d6b3b7a7d34983f8cf838526f7cc20
wConnor <connorga4@gmail.com>
2021-08-06 02:15:32 +0100

    added 'id' attribute to pomodoro

--- pomo.cpp
+++ pomo.cpp
@@ -1,5 +1,7 @@
 #include "pomo.hpp"
 
+Pomodoro::Pomodoro() { };
+
 Pomodoro::Pomodoro(std::string name, double time, double break_time, int count)
 {
 	this->name = name;
@@ -8,11 +10,14 @@ Pomodoro::Pomodoro(std::string name, double time, double break_time, int count)
 	this->count = count;
 }
 
+int Pomodoro::get_id() { return id; }
+void Pomodoro::set_id(int new_id) { id = new_id; }
+
 std::string Pomodoro::get_name() { return name; }
 void Pomodoro::set_name(std::string new_name) { name = new_name; }
 
 double Pomodoro::get_time() { return time; }
-void Pomodoro::set_time(double new_time) { time = new_time; } 
+void Pomodoro::set_time(double new_time) { time = new_time; }
 
 double Pomodoro::get_break_time() { return time; }
 void Pomodoro::set_break_time(double new_break_time) { break_time = new_break_time; }
--- pomo.hpp
+++ pomo.hpp
@@ -6,13 +6,18 @@
 class Pomodoro
 {
 private:
+	int id = 0;
 	std::string name = "";
 	double time = 0.0;
 	double break_time = 0.0;
 	int count = 0;
 
 public:
+	Pomodoro();
 	Pomodoro(std::string name, double time, double break_time, int count);
+
+	int get_id();
+	void set_id(int new_id);
 
 	std::string get_name();
 	void set_name(std::string new_name);
