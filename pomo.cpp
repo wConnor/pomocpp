301513c43ea83fc8461176c204964f9d1f122371
wConnor <connorga4@gmail.com>
2021-08-07 17:34:22 +0100

    fixed get_break_time() to return 'break_time'

--- pomo.cpp
+++ pomo.cpp
@@ -28,8 +28,8 @@ void Pomodoro::set_name(std::string new_name) { name = new_name; }
 double Pomodoro::get_time() { return time; }
 void Pomodoro::set_time(double new_time) { time = new_time; }
 
-double Pomodoro::get_break_time() { return time; }
+double Pomodoro::get_break_time() { return break_time; }
 void Pomodoro::set_break_time(double new_break_time) { break_time = new_break_time; }
 
-void Pomodoro::set_count(int new_count) { count = new_count; }
 int Pomodoro::get_count() { return count; }
+void Pomodoro::set_count(int new_count) { count = new_count; }
